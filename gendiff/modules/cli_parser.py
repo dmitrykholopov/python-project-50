8eec1c30f6edb2f58616170069620e34aa6c4f61
Dmitry Kholopov <dkholopov@yandex.ru>
2022-11-03 01:19:45 +0300

    refactoring gendiff.py cli_parser.py

---
+++ gendiff/modules/cli_parser.py
@@ -0,0 +1,11 @@
+import argparse
+
+
+def get_cli_parametres():
+    parser = argparse.ArgumentParser(
+        description = 'Compares two configuration files and shows a difference.'
+    )
+    parser.add_argument('first_file')
+    parser.add_argument('second_file')
+    parser.add_argument(dest='')
+    args = parser.parse_args()
---
+++ gendiff/scripts/gendiff_.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+from gendiff.modules.cli_parser import get_cli_parametres
+
+
+def main():
+    get_cli_parametres()
+
+
+if __name__ == '__main__':
+    main()
