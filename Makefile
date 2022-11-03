6fecd96dce9c308c7676b38b36129bf4a00a1796
Dmitry Kholopov <dkholopov@yandex.ru>
2022-11-04 01:47:20 +0300

    make STEP2 logic: add optional arguments

--- Makefile
+++ Makefile
@@ -16,7 +16,7 @@ test-coverage:
 	poetry run pytest --cov=gendiff --cov-report xml
 
 package-install:
-	python3 -m pip install --user /Users/dmitrykholopov/HEXLET/python-project-50/dist/hexlet_code-0.1.0-py3-none-any.whl
+	python3 -m pip install /Users/dmitrykholopov/HEXLET/python-project-50/dist/hexlet_code-0.1.0-py3-none-any.whl
 
 package-uninstall:
 	python3 -m pip uninstall /Users/dmitrykholopov/HEXLET/python-project-50/dist/hexlet_code-0.1.0-py3-none-any.whl
--- gendiff/modules/cli_parser.py
+++ gendiff/modules/cli_parser.py
@@ -2,10 +2,16 @@ import argparse
 
 
 def get_cli_parametres():
+    # Description for our script
     parser = argparse.ArgumentParser(
         description='Compares two configuration files and shows a difference.'
     )
-    parser.add_argument('first_file')
-    parser.add_argument('second_file')
-    parser.add_argument(dest='')
-    # args = parser.parse_args()
+    # Positional arguments
+    parser.add_argument('first_file', type=str)
+    parser.add_argument('second_file', type=str)
+    # Optional arguments, we can add DEFAULT value too or do it REQUARED
+    parser.add_argument('-f', '--format',
+                        help='set format of output',
+                        dest='FORMAT'
+                        )
+    parser.parse_args()
