c0fc444362779bf6b8bf54b40183c6d0e4807b3b
Dmitry Kholopov <dkholopov@yandex.ru>
2022-11-11 01:33:42 +0300

    make STEP3 logic

---
+++ gendiff/modules/gendiff.py
@@ -0,0 +1,33 @@
+import json
+
+
+def get_ev(value):
+    return json.JSONEncoder().encode(value)
+
+
+def generate_diff(file1_path, file2_path):
+    dict1 = json.load(open(file1_path))
+    dict2 = json.load(open(file2_path))
+    result_dict = dict(sorted({**dict1, **dict2}.items(), key=lambda x: x[0]))    
+    result = '}\n'
+    for key in result_dict:
+        if key in dict1 and key in dict2:
+            if dict1[key] == dict2[key]:
+                part = f'    {key}: {get_ev(dict1[key])}\n'
+                result += part
+            else:
+                part = f'  - {key}: {get_ev(dict1[key])}\n'
+                result += part
+                part = f'  + {key}: {get_ev(dict2[key])}\n'
+                result += part
+        if key in dict1 and key not in dict2:
+            part = f'  - {key}: {get_ev(dict1[key])}\n'
+            result += part
+        if key in dict2 and key not in dict1:
+            part = f'  + {key}: {get_ev(dict2[key])}\n'
+            result += part
+    result += '}'
+    return result
+
+
+print(generate_diff('gendiff/json_files/file1.json', 'gendiff/json_files/file2.json'))
--- gendiff/scripts/gendiff_.py
+++ gendiff/scripts/gendiff_.py
@@ -1,6 +1,32 @@
 #!/usr/bin/env python3
 
 from gendiff.modules.cli_parser import get_cli_parametres
+import json
+
+
+def generate_diff(file1_path, file2_path):
+    dict1 = json.load(open(file1_path))
+    dict2 = json.load(open(file2_path))
+    common_keys = sorted(tuple({**dict1, **dict2}.keys()))
+    result = '{\n'
+    for key in common_keys:
+        if key in dict1 and key in dict2:
+            if dict1[key] == dict2[key]:
+                part = f'    {key}: {dict1[key]}\n'
+                result += part
+            else:
+                part = f'  - {key}: {dict1[key]}\n'
+                result += part
+                part = f'  + {key}: {dict2[key]}\n'
+                result += part
+        if key in dict1 and key not in dict2:
+            part = f'  - {key}: {dict1[key]}\n'
+            result += part
+        if key in dict2 and key not in dict1:
+            part = f'  + {key}: {dict2[key]}\n'
+            result += part
+    result += '}'
+    return result
 
 
 def main():
