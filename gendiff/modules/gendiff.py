c0279f357dcef7270525e3c6deea94f2be20c333
newlif3r <newlif3r@newlif3rs-MB-Pro.local>
2023-06-07 01:23:02 +0300

    refactoring gendiff.py

--- gendiff/modules/gendiff.py
+++ gendiff/modules/gendiff.py
@@ -1,16 +1,14 @@
 import json
 
 
-def get_ev(value):
+def get_ev(value: str) -> str:
     return json.JSONEncoder().encode(value)
 
 
-def get_result_string(dict1, dict2, united_keys):
-    result = '}\n'
+def is_updated_or_unchanged(key: str, dict1: dict, dict2: dict) -> str:
+    result = ''
 
-    for key in united_keys:
-
-        if key in dict1 and key in dict2:
+    if key in dict1 and key in dict2:
             if dict1[key] == dict2[key]:
                 part = f'    {key}: {get_ev(dict1[key])}\n'
                 result += part
@@ -20,19 +18,45 @@ def get_result_string(dict1, dict2, united_keys):
                 part = f'  + {key}: {get_ev(dict2[key])}\n'
                 result += part
 
+    return result
+
+
+def is_removed(key: str, dict1: dict, dict2: dict) -> str:
+    result = ''
+    part = f'  - {key}: {get_ev(dict1[key])}\n'
+    result += part
+
+    return result
+
+
+def is_added(key: str, dict1: dict, dict2: dict) -> str:
+    result = ''
+    part = f'  + {key}: {get_ev(dict2[key])}\n'
+    result += part
+
+    return result
+
+
+def get_result_string(dict1: dict, dict2: dict, united_keys: set) -> str:
+    result = '}\n'
+
+    for key in united_keys:
+
+        if key in dict1 and key in dict2:
+            result += is_updated_or_unchanged(key, dict1, dict2)
+
         if key in dict1 and key not in dict2:
-            part = f'  - {key}: {get_ev(dict1[key])}\n'
-            result += part
+            result += is_removed(key, dict1, dict2)
 
         if key in dict2 and key not in dict1:
-            part = f'  + {key}: {get_ev(dict2[key])}\n'
-            result += part
+            result += is_added(key, dict1, dict2)
+
     result += '}'
 
     return result
 
 
-def generate_diff(file1_path, file2_path):
+def generate_diff(file1_path: str, file2_path: str) -> str:
     dict1 = json.load(open(file1_path))
     dict2 = json.load(open(file2_path))
     united_keys = sorted(set(dict1.keys()) | set(dict2.keys()))
